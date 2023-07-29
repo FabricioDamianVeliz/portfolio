65f57c089e7e57240a2f392ce12a18afcd813fa6
FabricioDamianVeliz <fabricio.damian.veliz@gmail.com>
2023-07-29 16:37:05 -0300

    Cambios:
    Creé el nav-bar, el header y el banner

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavBarComponent } from './components/nav-bar/nav-bar.component';
+import { BannerComponent } from './components/banner/banner.component';
+import { HeaderComponent } from './components/header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavBarComponent,
+    BannerComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
